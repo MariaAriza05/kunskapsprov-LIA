08ed01d29083c7215ea36426e65d4d0d455eb049
MariaAriza05 <mariafernandaariza@hotmail.es>
2026-02-17 00:17:38 +0100

    API test

---
+++ tests/test_api.py
@@ -0,0 +1,19 @@
+import requests
+
+def test_find_available_pets_returns_list():
+    url = "https://petstore.swagger.io/v2/pet/findByStatus"
+    params = {"status": "available"}
+
+    response = requests.get(url, params=params)
+
+    # 1. API svarar
+    assert response.status_code == 200
+
+    # 2. Svaret är JSON och en lista
+    data = response.json()
+    assert isinstance(data, list)
+
+    # 3. Det ska finnas minst 1 pet
+    assert len(data) > 0
+
+
